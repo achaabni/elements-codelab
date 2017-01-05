0fc0c4662631f991eec5a28afb5ef3fbffd83d6a
Marcus Hellberg <marcus@vaadin.com>
2017-01-05 15:25:47 -0800

    all but last step finished

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -12,9 +12,12 @@
 
 module.exports = {
   staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'index.html',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'images/**/*'
   ],
   navigateFallback: 'index.html',
+  stripPrefix: '',
+  replacePrefix: '.',
 };
